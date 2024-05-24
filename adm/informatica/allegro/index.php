6b68e9fab8214c00d6055cb463547c4819752e73
RESTA GIOVANNI <giovanni.resta@adm.gov.it>
2024-05-24 15:37:56 +0200

    update all

---
+++ adm/informatica/allegro/index.php
@@ -0,0 +1,18 @@
+<html>
+  <head>
+<title>Programmare con Allegro</title>
+<link rel="stylesheet" href="../../style.css" type="text/css" />
+  </head>
+<body>
+<h1>Programmare con Allegro</h1>
+
+<ol>
+<li><a href="intro.html">Introduzione e Installazione</a></li>
+<li><a href="helloworld.html">Primo programma</a></li>
+<li><a href="secondo.html">Secondo programma</a></li>
+</ol>
+
+</body>
+
+</html> 
+ 
