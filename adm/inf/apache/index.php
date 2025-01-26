1f46108c2fe1971066997ebf023465d483b3cbb5
cadmio <giovannirestadev@gmail.com>
2025-01-26 17:11:57 +0100

    update all

---
+++ adm/inf/apache/index.php
@@ -0,0 +1,18 @@
+<html>
+  <head>
+<title>Apache2</title>
+<link rel="stylesheet" href="../../style.css" type="text/css" />
+  </head>
+<body>
+
+<h1>Apache2</h1>
+
+<ol>
+<li><a href="intro.html">Introduzione ad Apache2</a></li>
+
+</ol>
+
+</body>
+
+</html> 
+ 
---
+++ adm/inf/crittografia/index.php
@@ -0,0 +1,18 @@
+<html>
+  <head>
+<title>Pagina principale</title>
+<link rel="stylesheet" href="../../style.css" type="text/css" />
+  </head>
+<body>
+
+
+
+<ol>
+<li><a href="hash.html">Password Hashing</a></li>
+<li><a href="rsa.html">RSA Algorithm</a></li>
+</ol>
+
+</body>
+
+</html> 
+ 
